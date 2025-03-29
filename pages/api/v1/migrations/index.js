57f2036eb2587a8acf7f6a76ed721f0c7513b211
Yuri Gonçalves <yuri_rjx@hotmail.com>
2025-03-28 23:47:38 -0300

    Update  endpoint to allow specific HTTP methods

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,8 +3,17 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+  }
+
+  let dbClient;
   try {
+    dbClient = await database.getNewClient();
+
     const defaultMigrationOptions = {
       dbClient,
       dryRun: true,
@@ -31,10 +40,9 @@ export default async function migrations(request, response) {
 
       response.status(200).json(migratedMigrations);
     }
-
-    return response.status(405).end();
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await dbClient.end();
   }
