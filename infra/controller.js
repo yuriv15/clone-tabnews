ad31189b407fbd31c7347444e93a7b7865b867f6
Yuri Gonçalves <yuri_rjx@hotmail.com>
2025-04-18 17:28:24 -0300

    refactor: use `next-connect` in `/migrations` and `/status`

---
+++ infra/controller.js
@@ -0,0 +1,24 @@
+import { InternalServerError, MethodNotAllowedError } from "./errors";
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+function onErrorHandler(error, request, response) {
+  const publicErrorObject = new InternalServerError({
+    statusCode: error.statusCode,
+    cause: error,
+  });
+
+  console.error(publicErrorObject);
+
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+export default {
+  errorHandlers: {
+    onNoMatch: onNoMatchHandler,
+    onError: onErrorHandler,
+  },
+};
