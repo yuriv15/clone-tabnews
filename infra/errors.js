1b47c57fe3c2a814c358f47c529c1d754cddb9b0
Yuri Gonçalves <yuri_rjx@hotmail.com>
2025-04-21 21:01:39 -0300

    feat: create and use `ValidationError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -37,6 +37,7 @@ export class ServiceError extends Error {
     };
   }
 }
+
 export class MethodNotAllowedError extends Error {
   constructor() {
     super("Método não permitido para este endpoint.");
@@ -44,6 +45,26 @@ export class MethodNotAllowedError extends Error {
     this.action =
       "Verifique se o método HTTP enviado é válido para este endpoint.";
     this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Um erro de validação ocorreu.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Ajuste os dados enviados e tente novamente.";
+    this.statusCode = 400;
   }
 
   toJSON() {
